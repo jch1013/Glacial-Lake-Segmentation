f3f38ba93083f7c1c80fa636617626070748b963
Jackson <jchayward1@gmail.com>
2025-04-20 20:43:02 -0700

    updata structure

--- get_data.py
+++ get_data.py
@@ -8,6 +8,9 @@ import numpy as np
 import requests
 from typing import Any
 import imageio
+import cv2
+import json
+
 
 
 from sentinelhub import (
@@ -45,12 +48,17 @@ lower_left = (-121.534044, 46.441355)
 
 
 class SentinelTileDownloader:
-    def __init__(self, tile_size_km, lon_lower_left, lat_lower_left, lon_upper_right, lat_upper_right, client_id = None, client_secret = None):
+    def __init__(self, tile_size_km, lon_lower_left, lat_lower_left, lon_upper_right, lat_upper_right, filepath, client_id = None, client_secret = None):
         self.tile_size_km = tile_size_km
         self.lon_lower_left = lon_lower_left
         self.lat_lower_left = lat_lower_left
         self.lon_upper_right = lon_upper_right
         self.lat_upper_right = lat_upper_right
+        self.data_folder = filepath
+
+        if not os.path.exists(self.data_folder):
+            print(f'file path not found. Creating new folder at {self.data_folder}')
+            os.mkdir(self.data_folder)
 
         # verify passed coordiniates are within the valid ranges for lat and lon
         if not (-180 <= lon_lower_left <= 180 and -90 <= lat_lower_left <= 90):
@@ -108,7 +116,7 @@ class SentinelTileDownloader:
         bbox_coordinates = (lon, lat, upper_right_lon, upper_right_lat)
         return BBox(bbox=bbox_coordinates, crs=CRS.WGS84)
 
-    def get_image_square(self, lon, lat, length):
+    def get_image_square(self, lon, lat):
         """ 
         Accepts longitude in float form
         Accepts latitude in float form
@@ -116,7 +124,7 @@ class SentinelTileDownloader:
         returns satellite image of box with side length length, and upper left corner of image at the passed coordinates
         """
         config = sentinelhub.SHConfig()
-        bbox = self.bbox_square(lon, lat, length)
+        bbox = self.bbox_square(lon, lat, self.tile_size_km)
         evalscript_true_color = """
         //VERSION=3
 
@@ -151,9 +159,10 @@ class SentinelTileDownloader:
         )
         true_color_imgs = request_true_color.get_data()
         image = true_color_imgs[0]
+
         return image
 
-    def canvas_region_rectangular(self, lon_lower_left, lat_lower_left, lon_upper_right, lat_upper_right, tile_size=2):
+    def canvas_region_rectangular(self):
         """
         Accepts lon and lat for the lower left corner of the desired region.
         Accepts lon and lat for upper right corner of the desired region
@@ -162,7 +171,7 @@ class SentinelTileDownloader:
         """
 
         def get_bbox_row(ll_lon, ll_lat, ur_lon):
-            last_bbox = self.bbox_square(ll_lon, ll_lat, tile_size)
+            last_bbox = self.bbox_square(ll_lon, ll_lat, self.tile_size_km)
             row = [last_bbox]
 
             while last_bbox.upper_right[0] < ur_lon:
@@ -174,24 +183,24 @@ class SentinelTileDownloader:
                 bbox_ll_lat = last_bbox.lower_left[1]
 
                 # create and append a new bounding box to the row
-                new_bbox = self.bbox_square(bbox_ll_lon, bbox_ll_lat, tile_size)
+                new_bbox = self.bbox_square(bbox_ll_lon, bbox_ll_lat, self.tile_size_km)
                 row.append(new_bbox)
             
             # add one final bbox to row to ensure complete coverate of the provided coordinates
             last_bbox = row[-1]
             bbox_ll_lon = last_bbox.upper_right[0]
             bbox_ll_lat = last_bbox.lower_left[1]
-            new_bbox = self.bbox_square(bbox_ll_lon, bbox_ll_lat, tile_size)
+            new_bbox = self.bbox_square(bbox_ll_lon, bbox_ll_lat, self.tile_size_km)
             row.append(new_bbox)
 
             return row
         
-        first_row = get_bbox_row(lon_lower_left, lat_lower_left, lon_upper_right)
+        first_row = get_bbox_row(self.lon_lower_left, self.lat_lower_left, self.lon_upper_right)
         bbox_grid = [first_row]
-        last_lat = lat_lower_left
+        last_lat = self.lat_lower_left
 
 
-        while last_lat < lat_upper_right:
+        while last_lat < self.lat_upper_right:
 
             # for each new row, get the lower left and upper right bbox coordinates for the first element of the prior row
             last_lower_left_row_start = bbox_grid[-1][0].lower_left
@@ -202,7 +211,7 @@ class SentinelTileDownloader:
             new_lower_left_lat = last_upper_right_row_start[1]
 
             # get the next row of the area and append it to the row
-            next_row = get_bbox_row(new_lower_left_lon, new_lower_left_lat, lon_upper_right)
+            next_row = get_bbox_row(new_lower_left_lon, new_lower_left_lat, self.lon_upper_right)
             bbox_grid.append(next_row)
 
             last_lat = new_lower_left_lat
@@ -212,12 +221,12 @@ class SentinelTileDownloader:
         last_upper_right_row_start = bbox_grid[-1][0].upper_right
         new_lower_left_lon = last_lower_left_row_start[0]
         new_lower_left_lat = last_upper_right_row_start[1]
-        next_row = get_bbox_row(new_lower_left_lon, new_lower_left_lat, lon_upper_right)
+        next_row = get_bbox_row(new_lower_left_lon, new_lower_left_lat, self.lon_upper_right)
         bbox_grid.append(next_row)
 
         return bbox_grid
 
-    def get_images_from_region(self, region_lon_lower_left, region_lat_lower_left, region_lon_upper_right, region_lat_upper_right, output_dir, tile_size=2):
+    def get_images_from_region(self):
         """
         Downloads satellite images for a rectangular region defined by lower-left and upper-right coordinates.
 
@@ -250,9 +259,10 @@ class SentinelTileDownloader:
         - Prints progress for each processed tile.
         """
 
-        bounding_box_array_2d = self.canvas_region_rectangular(region_lon_lower_left, region_lat_lower_left, region_lon_upper_right, region_lat_upper_right, tile_size=2)
+        bounding_box_array_2d = self.canvas_region_rectangular()
         total_images = len(bounding_box_array_2d) * len(bounding_box_array_2d[0])
         processed = 0
+        image_data = []
 
         # iterate through the 2d array of bounding boxes and download the image for each bounding box
         for row in range(len(bounding_box_array_2d)):
@@ -260,32 +270,47 @@ class SentinelTileDownloader:
                 bbox = bounding_box_array_2d[row][col]
                 file_name = f'{bbox.lower_left[0]},{bbox.lower_left[1]}'
 
-                filepath = os.path.join(output_dir, file_name)
+                filepath = os.path.join(self.data_folder, file_name)
 
                 # check if file already exists before calling sentinel api
-                if not os.path.exists(filepath + '.png'):
-                    img = self.get_image_square(bbox.lower_left[0], bbox.lower_left[1], tile_size)
-                    self.save_image(img, filepath=filepath + '.png', factor=5 / 255, clip_range=(0, 1))
+                png_filepath = filepath + '.png'
+                if not os.path.exists(png_filepath):
+                    img = self.get_image_square(bbox.lower_left[0], bbox.lower_left[1])
+                    self.save_image(img, bbox, png_filepath)
+
+                    # create entry for json file to keep track of image locations on map
+                    json_data = {
+                        'image_path' : png_filepath, 
+                        "bbox_lower_left" : bbox.lower_left,
+                        "bbox_upper_right" : bbox.upper_right
+                    }
+
+                    image_data.append(json_data)
                 processed += 1
                 print(f'Processed {processed} / {total_images}')
 
-    def save_image(self, image: np.ndarray, filepath: str, factor: float = 1.0, clip_range: tuple[float, float] | None = None, **kwargs: Any) -> None:
+        # write json file
+        with open(self.data_folder + "/images.json", "w") as f:
+            json.dump(image_data, f, indent=2)
+
+    def save_image(self, image, bbox, filepath) :
     
-        _, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 15))
-        if clip_range is not None:
-            ax.imshow(np.clip(image * factor, *clip_range), **kwargs)
-        else:
-            ax.imshow(image * factor, **kwargs)
 
-        ax.axis('off')
+        # Ensure the image is in the correct format (uint8)
+        #p2, p95 = np.percentile(image, (2, 98))
+        #image = np.clip(image, p2, p95)
+        #image = (image - p2) / (p95 - p2)
+        #image = (image * 255).astype(np.uint8)
+        image = image[..., ::-1]
 
+        # Save the image using OpenCV
         try:
-            print(image.size)
-            #plt.savefig(filepath, bbox_inches='tight', pad_inches=0)
+            cv2.imwrite(filepath, image)
         except Exception as e:
-            print(f'Error: {e}')
+            print(f"Error saving image: {e}")
 
-lower_left = ( -120.904060, 48.162154)
-upper_right = ( -120.894558, 48.188148)
-data_getter = SentinelTileDownloader(2, lower_left[0], lower_left[1], upper_right[0], upper_right[1])
-
+lower_left = (-123.826859, 47.509866)
+upper_right = (-123.031568, 47.995574)
+test_path = '/Users/jacksonhayward/Desktop/Olympic_Images'
+data_getter = SentinelTileDownloader(2, lower_left[0], lower_left[1], upper_right[0], upper_right[1], test_path)
+data_getter.get_images_from_region()
