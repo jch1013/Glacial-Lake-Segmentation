ca802cf7d4007ba390409e277a607066df5a5ea6
Jackson <jchayward1@gmail.com>
2025-05-10 17:45:59 -0500

    refactor preprocess, and remove patchify funcitonality

--- preprocess.py
+++ preprocess.py
@@ -5,111 +5,88 @@ from matplotlib import pyplot as plt
 from patchify import patchify
 from PIL import Image
 import tensorflow as tf
-from tensorflow.keras.metrics import MeanIoU
 os.environ["SM_FRAMEWORK"] = "tf.keras"
 import segmentation_models as sm
 from sklearn.preprocessing import MinMaxScaler
+from util import compare_images
 
 
-scaler = MinMaxScaler()
-patch_size = 256
-mask_dir = 'train_mini/masks'
-data_dir = 'train_mini'
+class preprocesser:
+    def __init__(self, root_directory):
+        self.root_dir = root_directory
+        self.scaler = MinMaxScaler()
 
-def generate_empty_mask(path_to_image):
-    image_filename = path_to_image.split(os.path.sep)[-1]
-    image_filename = image_filename.split('.')[0]
-    mask_filename = os.path.join(mask_dir, image_filename + '.tif')
-    if os.path.exists(mask_filename): return
-    image = cv2.imread(path_to_image)
-    height, width, channels = image.shape
-    mask = np.zeros((height, width), dtype=np.uint8)
-    cv2.imwrite(mask_filename, mask)
-
-def fill_masks(data_dir):
-    image_dir = os.path.join(data_dir, 'images')
-    mask_dir = os.path.join(data_dir, 'masks')
-
-    image_dir_files = os.listdir(image_dir)
-    for image in image_dir_files:
-        mask_name = image.replace('.png', '.tif')
-        if not os.path.exists(os.path.join(mask_dir, mask_name)):
-            image_path = os.path.join(image_dir, image)
-            generate_empty_mask(image_path)
-
-def get_image_dataset(blur=False):
-    image_dataset = []
-    data_dir = 'train_mini'
-    for path, subdirs, files in os.walk(data_dir):
-        dirname = path.split(os.path.sep)[-1]
-        if dirname == 'images':   #Find all 'images' directories
-            images = sorted(os.listdir(path))
-            for i, image_name in enumerate(images):  
-                if image_name.endswith(".png"):   #Only read png images...
-                
-                    image = cv2.imread(path+"/"+image_name, 1)  #Read each image as BGR
-                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    def generate_empty_mask(self, path_to_image):
+        image_filename = path_to_image.split(os.path.sep)[-1]
+        image_filename = image_filename.split('.')[0]
+        mask_filename = os.path.join(self.root_dir, 'masks', image_filename + '.tif')
+        if os.path.exists(mask_filename): return
+        image = cv2.imread(path_to_image)
+        height, width, channels = image.shape
+        mask = np.zeros((height, width), dtype=np.uint8)
+        cv2.imwrite(mask_filename, mask)
 
 
+    def fill_missing_masks(self):
+        image_dir = os.path.join(self.root_dir, 'images')
+        mask_dir = os.path.join(self.root_dir, 'masks')
 
-                    SIZE_X = (image.shape[1]//patch_size)*patch_size #Nearest size divisible by our patch size
-                    SIZE_Y = (image.shape[0]//patch_size)*patch_size #Nearest size divisible by our patch size
-                    image = Image.fromarray(image)
-                    image = image.crop((0 ,0, SIZE_X, SIZE_Y))  #Crop from top left corner
-                    image = np.array(image)
+        image_dir_files = os.listdir(image_dir)
+        for image in image_dir_files:
+            mask_name = image.replace('.png', '.tif')
+            if not os.path.exists(os.path.join(mask_dir, mask_name)):
+                image_path = os.path.join(image_dir, image)
+                self.generate_empty_mask(image_path)
+
+    def get_image_dataset(self, blur=True):
+        image_dataset = []
+        for path, subdirs, files in os.walk(self.root_dir):
+            dirname = path.split(os.path.sep)[-1]
+            if dirname == 'images':   #Find all 'images' directories
+                images = sorted(os.listdir(path))
+                for i, image_name in enumerate(images):  
+                    if image_name.endswith(".png"):   #Only read png images...
                     
-                    # apply blur if needed
-                    if blur:
-                        image = cv2.GaussianBlur(image, (3, 3), 0)
-        
-                    #Extract patches from each image
-                    print("Now patchifying image:", path+"/"+image_name)
-                    patches_img = patchify(image, (patch_size, patch_size, 3), step=patch_size)  #Step=256 for 256 patches means no overlap
-            
-                    for i in range(patches_img.shape[0]):
-                        for j in range(patches_img.shape[1]):
-                            
-                            single_patch_img = patches_img[i,j,:,:]
-                            
-                            #Use minmaxscaler instead of just dividing by 255. 
-                            single_patch_img = scaler.fit_transform(single_patch_img.reshape(-1, single_patch_img.shape[-1])).reshape(single_patch_img.shape)
-                            
-                            #single_patch_img = (single_patch_img.astype('float32')) / 255. 
-                            single_patch_img = single_patch_img[0] #Drop the extra unecessary dimension that patchify adds.
-                            image_dataset.append(single_patch_img)
-    return np.array(image_dataset)
+                        image_path = path+"/"+image_name
+                        image = cv2.imread(image_path, 1)  #Read each image as BGR
+                        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                        image = Image.fromarray(image)
+                        image = np.array(image)
+                        
+                        # FIX THIS LATER - STANDARDIZE IMAGE SHAPES WHEN DOWNLOADING TO AVOID DISTORTING
+                        image = cv2.resize(image, (1024, 1024))
+                        
+                        # apply blur if needed
+                        if blur:
+                            image = cv2.GaussianBlur(image, (3, 3), 0)
+                        
+                        scaled_image = self.scaler.fit_transform(image.reshape(-1, image.shape[-1])).reshape(image.shape)
+                        image_dataset.append(scaled_image)
+                        print(f'Loaded image {image_path}')
+        return np.array(image_dataset)
+    
+    def get_mask_dataset(self, blur=True):
+        self.fill_missing_masks()
+        mask_dataset = []  
+        for path, subdirs, files in os.walk(self.root_dir):
+            dirname = path.split(os.path.sep)[-1]
+            if dirname == 'masks':   #Find all 'images' directories
+                masks = sorted(os.listdir(path))
+                for i, mask_name in enumerate(masks):  
+                    if mask_name.endswith(".tif"):   #Only read png images... (masks in this dataset)
+                    
+                        mask = cv2.imread(path+"/"+mask_name, 0)  #Read each image as Grey (or color but remember to map each color to an integer)
+                        mask = mask // 255
+                        mask = np.array(mask) 
 
-def get_mask_dataset(blur=False):
-    fill_masks(data_dir)
-    mask_dataset = []  
-    for path, subdirs, files in os.walk(data_dir):
-        dirname = path.split(os.path.sep)[-1]
-        if dirname == 'masks':   #Find all 'images' directories
-            masks = sorted(os.listdir(path))
-            for i, mask_name in enumerate(masks):  
-                if mask_name.endswith(".tif"):   #Only read png images... (masks in this dataset)
-                
-                    mask = cv2.imread(path+"/"+mask_name, 0)  #Read each image as Grey (or color but remember to map each color to an integer)
-                    mask = mask // 255
-                    SIZE_X = (mask.shape[1]//patch_size)*patch_size #Nearest size divisible by our patch size
-                    SIZE_Y = (mask.shape[0]//patch_size)*patch_size #Nearest size divisible by our patch size
-                    mask = Image.fromarray(mask)
-                    mask = mask.crop((0 ,0, SIZE_X, SIZE_Y))  #Crop from top left corner
-                    mask = np.array(mask) 
+                        # FIX LATER - STANDARDIZE SHAPE IN DATA PREPROCESSING
+                        mask = cv2.resize(mask, (1024, 1024))
 
-                    # apply blur if needed
-                    if blur:
-                        mask = cv2.GaussianBlur(mask, (3, 3), 0)            
-        
-                    #Extract patches from each image
-                    print("Now patchifying mask:", path+"/"+mask_name)
-                    patches_mask = patchify(mask, (patch_size, patch_size), step=patch_size)  #Step=256 for 256 patches means no overlap
+                        # apply blur if needed
+                        if blur:
+                            mask = cv2.GaussianBlur(mask, (3, 3), 0)            
             
-                    for i in range(patches_mask.shape[0]):
-                        for j in range(patches_mask.shape[1]):
-                            
-                            single_patch_mask = patches_mask[i,j,:,:]
-                            # single_patch_mask = single_patch_mask[0] #Drop the extra unecessary dimension that patchify adds.                               
-                            mask_dataset.append(single_patch_mask)
-    return np.array(mask_dataset)
+                        mask_dataset.append(mask)
+        return np.array(mask_dataset)
+    
 
